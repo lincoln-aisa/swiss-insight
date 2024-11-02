3f77ebd0ee8d6f2a79dd2828315d459d5aee0028
lincoln-aisa <lincoln.aisa@yahoo.com>
2024-11-02 16:08:40 +0100

    added chatbot input logo and submit button logo

--- src/components/QuestionInput/QuestionInput.js
+++ src/components/QuestionInput/QuestionInput.js
@@ -1,16 +1,23 @@
 import "./questionInput.css";
+import submit_icon from "./submit_button.png"
+import chatbot_icon from "./chatbot_icon.png"
 
 function QuestionInput() {
   return (
-    <div className="question-input">
-      {/*<img />*/}
-      <input 
-        className="question-input-field"
-        type= "search"
-        placeholder= "Hi, I'm Lora, I am still under development"
-      />
-      <button></button>
-    </div>
+    <main className="question-main">
+      
+      <form className="input-form">
+        <img src={chatbot_icon} alt="chatbot icon"/>
+        <input 
+          className="question-input-field"
+          type= "search"
+          placeholder= "Hi, I'm Lora, I am still under development"
+        />
+        <button>
+          <img src={submit_icon} alt="submit button icon" />
+        </button>
+      </form>
+    </main>
   );
 }
 export default QuestionInput;
